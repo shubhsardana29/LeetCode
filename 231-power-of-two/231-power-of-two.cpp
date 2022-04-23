98fc28b2cd2ee9814108e0fe58e9371c29b833c6
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2022-04-23 06:31:29 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (70.65%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -4,7 +4,23 @@ public:
         if (n<=0){
             return 0;
         }
-        int ans= n & n-1;
-        return (ans==0);
+        int count=0;
+        while(n!=0){
+            //check last bit
+            if (n & 1){
+                count++;
+            }
+            // after checking bit, just remove it from n
+            n=n>>1;
+        }
+        
+        // 2 ki power wale no. has only 1 set bit
+        if (count==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
     }
 };
