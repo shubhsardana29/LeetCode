4f1bb1129a141e06bc340abb37546a60b8902696
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2023-11-16 06:59:14 +0530

    Time: 7 ms (29.80%) | Memory: 8.6 MB (46.58%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -10,16 +10,37 @@
  */
 class Solution {
 public:
+    ListNode* reverseUsingRecursion(ListNode* prev , ListNode *curr) {
+
+        //base case
+        if(curr == NULL){
+            return prev;
+        }
+
+        ListNode *nextNode=curr->next;
+        // 1 case khud ka
+            curr->next=prev;
+        
+        // baaki recursion se
+        ListNode* RecursionAns = reverseUsingRecursion(curr,nextNode);
+
+        return RecursionAns;
+
+    }
     ListNode* reverseList(ListNode* head) {
         ListNode *prev = NULL;
         ListNode *curr = head;
 
-        while(curr!=NULL){
-            ListNode *nextNode=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=nextNode;
-        }
-        return prev;
+        return reverseUsingRecursion(prev,curr);
+    //     ListNode *prev = NULL;
+    //     ListNode *curr = head;
+
+    //     while(curr!=NULL){
+    //         ListNode *nextNode=curr->next;
+    //         curr->next=prev;
+    //         prev=curr;
+    //         curr=nextNode;
+    //     }
+    //     return prev;
     }
 };
