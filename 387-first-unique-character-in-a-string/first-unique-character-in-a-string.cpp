01dc4ebbc7ae36d64f720612becd01c764cc2071
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2023-12-05 19:46:57 +0530

    Time: 68 ms (5.07%) | Memory: 12.4 MB (5.04%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -2,34 +2,27 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         int n = s.size();
-        char ans = ' ';
         map<char, int> freq;
         queue<char> q;
 
         for (int i = 0; i < n; i++) {
             char letter = s[i];
-            freq[letter]++;
+            freq[letter-'a']++;
             q.push(letter);
         }
 
         while (!q.empty()) {
             char letter = q.front();
-            if (freq[letter] == 1) {
-                ans = letter;
-                break;
+            if (freq[letter-'a'] == 1) {
+                // Return the index of the first non-repeating character
+                return s.find(letter);
+
             } else {
                 q.pop();
             }
         }
 
-        // Find the index of the first occurrence of the unique character
-        for (int i = 0; i < n; i++) {
-            if (s[i] == ans) {
-                return i;
-            }
-        }
-
-        // If no unique character is found
+         // If no unique character is found
         return -1;
     }
 };
