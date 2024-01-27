b918f50cab224cccb3077cc40e7b99cc9dacf0f7
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2024-01-27 11:08:01 +0530

    Time: 121 ms (81.27%) | Memory: 68.9 MB (65.81%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void heapify(vector<int>& nums, int n, int index) {
+        int leftIndex = 2 * index + 1; // 1-based indexing correction
+        int rightIndex = 2 * index + 2; // 1-based indexing correction
+        int largestKaIndex = index;
+
+        // Compare with the values at leftIndex and rightIndex,
+        if (leftIndex < n && nums[leftIndex] > nums[largestKaIndex])
+            largestKaIndex = leftIndex;
+        if (rightIndex < n && nums[rightIndex] > nums[largestKaIndex])
+            largestKaIndex = rightIndex;
+
+        if (index != largestKaIndex) {
+            swap(nums[index], nums[largestKaIndex]);
+            index = largestKaIndex;
+            heapify(nums, n, index);
+        }
+    }
+
+    void heapsort(vector<int>& nums, int n) {
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(nums, n, i);
+        }
+        
+        for (int i = n - 1; i > 0; i--) {
+            swap(nums[0], nums[i]);
+            heapify(nums, i, 0);
+        }
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+
+        
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(nums, n, i);
+        }
+
+        heapsort(nums, n);
+
+        return nums;
+    }
+};
