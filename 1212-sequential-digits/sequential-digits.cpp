13bfa30f02a466287f86a66804c4d5793242a31c
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2024-02-02 20:26:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (8.37%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -1,11 +1,34 @@
+// class Solution {
+// public:
+//     vector<int> sequentialDigits(int low, int high) {
+//         vector<int>ans;
+//         vector<int> possible = {12,23,34,45,56,67,78,89,123,234,345,456,567,678,789,1234,2345,3456,4567,5678,6789,12345,23456,34567,45678,56789,123456,234567,345678,456789,1234567,2345678,3456789,12345678,23456789,123456789};
+//         for(int i = 0; i < possible.size(); i++) {
+//             if(low <= possible[i] && high >= possible[i]) {
+//                 ans.push_back(possible[i]);
+//             }
+//         }
+
+//         sort(ans.begin(), ans.end());
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        vector<int>ans;
-        vector<int> possible = {12,23,34,45,56,67,78,89,123,234,345,456,567,678,789,1234,2345,3456,4567,5678,6789,12345,23456,34567,45678,56789,123456,234567,345678,456789,1234567,2345678,3456789,12345678,23456789,123456789};
-        for(int i = 0; i < possible.size(); i++) {
-            if(low <= possible[i] && high >= possible[i]) {
-                ans.push_back(possible[i]);
+        vector<int> ans;
+
+        string digits = "123456789";
+        int n = digits.size();
+
+        for (int len = 1; len <= n; ++len) {
+            for (int i = 0; i <= n - len; ++i) {
+                string sub = digits.substr(i, len);
+                int num = stoi(sub);
+                if (num >= low && num <= high) {
+                    ans.push_back(num);
+                }
             }
         }
 
