9ed5c877fb71ba4d9b277379e8a215aa965c3787
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2024-02-04 16:17:57 +0530

    Time: 119 ms (80.23%) | Memory: 69.1 MB (66.67%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     void heapify(vector<int>& nums, int n, int index) {
-        int leftIndex = 2 * index + 1; // 1-based indexing correction
-        int rightIndex = 2 * index + 2; // 1-based indexing correction
+        int leftIndex = 2 * index + 1; // 0-based indexing correction
+        int rightIndex = 2 * index + 2; // 0-based indexing correction
         int largestKaIndex = index;
 
         // Compare with the values at leftIndex and rightIndex,
@@ -23,9 +23,14 @@ public:
             heapify(nums, n, i);
         }
         
-        for (int i = n - 1; i > 0; i--) {
-            swap(nums[0], nums[i]);
-            heapify(nums, i, 0);
+        // for (int i = n - 1; i > 0; i--) {
+        //     swap(nums[0], nums[i]);
+        //     heapify(nums, i, 0);
+        // }
+        while(n>0){
+            swap(nums[0],nums[n-1]);
+            n--;
+            heapify(nums,n,0);
         }
     }
 
