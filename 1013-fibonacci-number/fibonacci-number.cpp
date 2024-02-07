c51d6cab46985e14be1715d72d34b6d61f829edf
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2024-02-07 20:17:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (23.19%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -6,11 +6,16 @@ public:
         
         return solveUsingRecursion(n - 1) + solveUsingRecursion(n - 2); 
     }
+
     
+    // int fib(int n) {
+    //     //Step 1: create dp array
+    //     vector<int>dp(n+1,-1);
+    //     return solveUsingMem(n,dp);
+    // }
+
     int fib(int n) {
-        //Step 1: create dp array
-        vector<int>dp(n+1,-1);
-        return solveUsingMem(n,dp);
+        return solveUsingTabulation(n);
     }
 
     int solveUsingMem(int n,vector<int>& dp){
@@ -23,6 +28,30 @@ public:
         // Step 2: store and return using dp aray
         dp[n]= solveUsingMem(n - 1,dp) + solveUsingMem(n - 2,dp);
         return dp[n]; 
+    }
+    int solveUsingTabulation(int n){
+        //Step 1: CREATE DP ARRAY
+
+        vector<int>dp(n+1,-1);
+
+        // Step 2: analyze base case and fill dp aray
+        dp[0]=0;
+        if(n==0) return dp[0];
+        dp[1]=1;
+
+        // step 3: Fill the remaining dp array
+
+        // already filled index 0 & 1 in size of n+1 i.e loop was 0 to n
+        // so remaining size of loop = 2 -> n
+
+        for(int i=2;i<n+1;i++){
+            //copy paste the recursive logic
+            //replace recursive calls with dp array
+            // make sure dp array is using looping variable " i ";
+            dp[i]=dp[i - 1] + dp[i - 2];
+        }
+        //ans return karna hai
+        return dp[n];
     }
     /* dp
     1) top down - recursion + memoization
