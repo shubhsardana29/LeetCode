e87827a7a71e9074970324088bb9bf4613308bb6
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2024-02-09 03:53:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (38.05%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -21,27 +21,56 @@
 
 //Top down DP solution - recursion + memoization
 
+// class Solution {
+// public:
+//     int solveUsingTopDownDp(vector<int>& nums,int index,vector<int>&dp){
+//         // base case
+//         if(index>=nums.size()) return 0;
+
+//         //check if answer already exists
+//         if(dp[index]!=-1) return dp[index];
+
+        
+//         int robAmt1 = nums[index] + solveUsingTopDownDp(nums,index+2,dp);
+//         int robAmt2 = 0 + solveUsingTopDownDp(nums,index+1,dp);
+
+//         return dp[index]=max(robAmt1,robAmt2);
+//     }
+//     int rob(vector<int>& nums) {
+//         int index=0;
+//         int n=nums.size();
+//         vector<int>dp(n,-1);
+//         return solveUsingTopDownDp(nums,index,dp);
+        
+//     }
+// };
+
+//Bottom up DP solution - Tabulation method
+
 class Solution {
 public:
-    int solveUsingTopDownDp(vector<int>& nums,int index,vector<int>&dp){
-        // base case
-        if(index>=nums.size()) return 0;
+    int solveUsingBottomUpDp(vector<int>& nums){
+        //create dp aaray
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        // analyze base case and fill initial dp array
+         dp[n-1]=nums[n-1];
 
-        //check if answer already exists
-        if(dp[index]!=-1) return dp[index];
+        //fill remaining dp array : analyzie loop initalization conditions
+        for(int index=n-2;index>=0;index--){
+            int tempAns=0;
+            if(index+2<n){
+                tempAns=dp[index+2];   //to resolve out of bound error
+            }
+            int robAmt1 = nums[index] + tempAns;
+            int robAmt2 = 0 + dp[index+1];
+            dp[index]=max(robAmt1,robAmt2);
+        }
 
-        // sol for 1 case
-        int robAmt1 = nums[index] + solveUsingTopDownDp(nums,index+2,dp);
-        int robAmt2 = 0 + solveUsingTopDownDp(nums,index+1,dp);
-
-        return dp[index]=max(robAmt1,robAmt2);
+        return dp[0];
     }
     int rob(vector<int>& nums) {
-        int index=0;
-        int n=nums.size();
-        vector<int>dp(n+1,-1);
-        return solveUsingTopDownDp(nums,index,dp);
-        
+        return solveUsingBottomUpDp(nums); 
     }
 };
 
