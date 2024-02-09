b7af69072dd6af99cd43ebea4398f1dfad4500c9
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2024-02-09 18:07:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (56.97%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -47,30 +47,59 @@
 
 //Bottom up DP solution - Tabulation method
 
+// class Solution {
+// public:
+//     int solveUsingBottomUpDp(vector<int>& nums){
+//         //create dp aaray
+//         int n=nums.size();
+//         vector<int>dp(n,-1);
+//         // analyze base case and fill initial dp array
+//          dp[n-1]=nums[n-1];
+
+//         //fill remaining dp array : analyzie loop initalization conditions
+//         for(int index=n-2;index>=0;index--){
+//             int tempAns=0;
+//             if(index+2<n){
+//                 tempAns=dp[index+2];   //to resolve out of bound error
+//             }
+//             int robAmt1 = nums[index] + tempAns;
+//             int robAmt2 = 0 + dp[index+1];
+//             dp[index]=max(robAmt1,robAmt2);
+//         }
+
+//         return dp[0];
+//     }
+// }; 
+
+
+// space optimised technique
 class Solution {
 public:
-    int solveUsingBottomUpDp(vector<int>& nums){
-        //create dp aaray
+    int solveUsingBottomUpDpSpaceOpti(vector<int>& nums){
+        
         int n=nums.size();
-        vector<int>dp(n,-1);
-        // analyze base case and fill initial dp array
-         dp[n-1]=nums[n-1];
+        int prev=nums[n-1];
+        int curr=0;
+        int next=0;
 
-        //fill remaining dp array : analyzie loop initalization conditions
         for(int index=n-2;index>=0;index--){
             int tempAns=0;
             if(index+2<n){
-                tempAns=dp[index+2];   //to resolve out of bound error
+                tempAns=next;   //to resolve out of bound error
             }
             int robAmt1 = nums[index] + tempAns;
-            int robAmt2 = 0 + dp[index+1];
-            dp[index]=max(robAmt1,robAmt2);
+            int robAmt2 = 0 + prev;
+            curr=max(robAmt1,robAmt2);
+
+            //imp step
+            next=prev;
+            prev=curr;
         }
 
-        return dp[0];
+        return prev;
     }
     int rob(vector<int>& nums) {
-        return solveUsingBottomUpDp(nums); 
+        return solveUsingBottomUpDpSpaceOpti(nums); 
     }
 };
 
