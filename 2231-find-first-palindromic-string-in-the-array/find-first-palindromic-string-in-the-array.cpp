5cecca19f06b61ed24add3bdc114fc6be9938ad1
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2024-02-13 18:48:53 +0530

    Time: 56 ms (23.20%) | Memory: 29.9 MB (7.01%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        for (string s : words) {
+            string reversed = s;
+            reverse(reversed.begin(), reversed.end());
+            
+            if (s == reversed) {
+                return s;
+            }
+        }
+        return "";
+        
+    }
+};
