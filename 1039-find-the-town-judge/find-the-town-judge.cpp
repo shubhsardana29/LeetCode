001d12a75281ed17d78b49315eabc36a82cb8889
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2024-02-22 08:47:06 +0530

    Time: 110 ms (95.52%) | Memory: 64.2 MB (73.94%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,23 +1,45 @@
+// class Solution {
+// public:
+//     int findJudge(int n, vector<vector<int>>& trust) {
+//         vector<int> indegree(n + 1, 0); // indegree[i] represents the number of people who trust person i
+//         vector<int> outdegree(n + 1, 0); // outdegree[i] represents the number of people person i trusts
+
+//         for (const auto& relation : trust) {
+//             int a = relation[0];
+//             int b = relation[1];
+//             outdegree[a]++;
+//             indegree[b]++;
+//         }
+
+//         for (int i = 1; i <= n; ++i) {
+//             if (indegree[i] == n - 1 && outdegree[i] == 0) {
+//                 return i;
+//             }
+//         }
+
+//         return -1; // No town judge found
+
+//     }
+//};
+
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> indegree(n + 1, 0); // indegree[i] represents the number of people who trust person i
-        vector<int> outdegree(n + 1, 0); // outdegree[i] represents the number of people person i trusts
+        vector<int> degrees(n + 1, 0); // Positive values represent outdegree, negative values represent indegree
 
         for (const auto& relation : trust) {
             int a = relation[0];
             int b = relation[1];
-            outdegree[a]++;
-            indegree[b]++;
+            degrees[a]--; // Reduce outdegree for a
+            degrees[b]++; // Increase indegree for b
         }
 
         for (int i = 1; i <= n; ++i) {
-            if (indegree[i] == n - 1 && outdegree[i] == 0) {
+            if (degrees[i] == n - 1) {
                 return i;
             }
         }
 
         return -1; // No town judge found
-
     }
 };
