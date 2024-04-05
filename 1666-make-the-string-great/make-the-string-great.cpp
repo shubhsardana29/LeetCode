cf82895690742bc8d5ffa9d28e20a090a65c593c
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2024-04-05 19:26:02 +0530

    Time: 4 ms (42.41%) | Memory: 7.7 MB (74.45%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     string makeGood(string s) {
-        stack<char> stack;
-        
-        for (char c : s) {
-            if (!stack.empty() && abs(c - stack.top()) == 32) {
-                stack.pop(); // Remove the last character from the stack
+        int j = -1; 
+        for (int i = 0; i < s.size(); ++i) {
+            if (j >= 0 && abs(s[i] - s[j]) == 32) {
+                // If current and last valid characters form a bad pair, skip them
+                --j;
             } else {
-                stack.push(c); // Otherwise, add the current character to the stack
+                // Update last valid character and move both pointers forward
+                s[++j] = s[i];
             }
         }
-        
-        string good = "";
-        while (!stack.empty()) {
-            good = stack.top() + good; // Construct the resulting string in reverse order
-            stack.pop();
-        }
-        
-        return good;
+        // Construct the resulting string using substring from 0 to j+1
+        return s.substr(0, j + 1);
     }
 };
