6393e01847e35c8983b46c4aa4eb09995210b410
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2024-05-11 03:28:02 +0530

    Time: 225 ms (68.46%) | Memory: 12.3 MB (78.65%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
-    typedef pair<double,pair<int,int>>pp;
+    typedef pair<double, pair<int, int>> Fraction;
+
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        priority_queue<pp,vector<pp>,greater<pp>>pq;
-        int n=arr.size();
-        for(int i=0;i<n;i++){
-            pq.push({double(arr[i])/arr[n-1], {i, n-1}});
+        priority_queue<Fraction, vector<Fraction>, greater<Fraction>> pq;
+
+        int n = arr.size();
+        
+        for (int i = 0; i < n; i++) {
+            double value = double(arr[i]) / arr[n - 1];
+            pq.push({value, {i, n - 1}});
         }
-        for(int i=0;i<k-1;i++){
-            pp ans = pq.top();
+        
+        for (int i = 0; i < k - 1; i++) {
+            Fraction currentFraction = pq.top();
             pq.pop();
-            if(ans.second.second-1>ans.second.first){
-                pq.push({double(arr[ans.second.first]) / arr[ans.second.second - 1], {ans.second.first, ans.second.second - 1}});
+            
+            int numeratorIndex = currentFraction.second.first;
+            int denominatorIndex = currentFraction.second.second;
+
+            if (denominatorIndex - 1 > numeratorIndex) {
+                double nextValue = double(arr[numeratorIndex]) / arr[denominatorIndex - 1];
+                pq.push({nextValue, {numeratorIndex, denominatorIndex - 1}});
             }
         }
-        return {arr[pq.top().second.first],arr[pq.top().second.second]};
+        
+        return {arr[pq.top().second.first], arr[pq.top().second.second]};
     }
 };
