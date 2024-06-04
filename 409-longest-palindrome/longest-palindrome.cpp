4fe1ca77a276095f804f582a3ccf6e8d8737f1ee
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2024-06-04 06:47:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (75.88%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int longestPalindrome(string s) {
-        int count=0;
-        int n=s.size();
-        bool odd=false;
-
-        unordered_map<char,int>freq;
-        for(char ch:s){
+    int longestPalindrome(std::string s) {
+        int count = 0;
+        int freq[128] = {0}; 
+        
+        for (char ch : s) {
             freq[ch]++;
         }
-        // Calculate the length of the longest palindrome
-        for (auto it : freq) {
-            if (it.second % 2 == 0) { // even frequency of character
-                count += it.second;
-            } else { // odd frequency
-                count += it.second - 1;
-                odd = true;
+        
+        for (int i = 0; i < 128; ++i) {
+            count += (freq[i] / 2) * 2;  // Add the even part of the frequency
+            if (count % 2 == 0 && freq[i] % 2 == 1) {
+                count++;  // If count is even and we encounter an odd frequency, add one to allow for a central character
             }
         }
-        return odd==1 ? count+1 : count;
+        
+        return count;
     }
 };
