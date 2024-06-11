97fae9af808557d13488715b2c841bc5bedf8e51
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2024-06-11 01:46:52 +0000

    Time: 3 ms (53.89%) | Memory: 10.7 MB (6.31%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -0,0 +1,32 @@
+  class Solution {
+public:
+    // Define the type of the lambda function
+    using Comparator = function<bool(int, int)>;
+
+    // Define the lambda function as a class member
+    Comparator cmpr = [](int a, int b)
+    {
+        return a < b;
+    };
+
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        unordered_map<int, int> numPosMap;
+        for (int i = 0; i < arr2.size(); i++) {
+            numPosMap[arr2[i]] = i + 1;
+        }
+        
+        sort(arr1.begin(), arr1.end(), [&](int a, int b) {
+            if (numPosMap.count(a) && numPosMap.count(b)) {
+                return numPosMap[a] < numPosMap[b];
+            } else if (numPosMap.count(a)) {
+                return true;
+            } else if (numPosMap.count(b)) {
+                return false;
+            } else {
+                return a < b;
+            }
+        });
+        
+        return arr1;
+    }
+};
